ead4c1ef660f101aaf30865d40ab1cb6fdaf6da7
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:34 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ xoops_version.php
@@ -0,0 +1,14 @@
+<?php
+
+$modversion['name'] = 'meta';
+$modversion['version'] = '0.1';
+$modversion['description'] = 'meta balise gratuit avec www.totalgratuit.com';
+$modversion['credits'] = '--(bonjovi)--<br>( http://www.totalgratuit.com/ ) ';
+$modversion['help'] = 'no';
+$modversion['license'] = 'no';
+$modversion['official'] = 0;
+$modversion['image'] = 'images/meta.jpg';
+$modversion['dirname'] = 'meta';
+
+// Menu
+$modversion['hasMain'] = 1;
